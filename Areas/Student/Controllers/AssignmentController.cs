65db2c3e6ec7f00818b91c002c557dcce8741563
Pea Shooter <hakguy6@gmail.com>
2026-01-09 22:35:50 +0700

    merge 2.4

---
+++ Areas/Student/Controllers/AssignmentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EnglishCenterMVC.Services;
+using System.Threading.Tasks;
+
+namespace EnglishCenterMVC.Areas.Student.Controllers
+{
+    [Area("Student")]
+    public class AssignmentController : Controller
+    {
+        IAssignmentService assignmentService;
+
+        public AssignmentController(IAssignmentService assignmentService)
+        {
+            this.assignmentService = assignmentService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var assignments = await assignmentService.GetAssignmentsAsync();
+                return View(assignments);
+            } catch
+            {
+                return BadRequest();
+            }
+        }
+
+        public async Task<IActionResult> Details(int courseId)
+        {
+            if (courseId <= 0) return BadRequest();
+            try
+            {
+                var assigned = await assignmentService.GetAssignmentsAsync(courseId);
+                return View(assigned);
+            } catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
