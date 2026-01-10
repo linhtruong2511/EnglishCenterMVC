c78dde64f4346e6d6b38e306c0e1bf65887f180c
Pea Shooter <hakguy6@gmail.com>
2026-01-10 15:08:03 +0700

    m

--- Areas/Student/Controllers/SubmissionController.cs
+++ Areas/Student/Controllers/SubmissionController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using EnglishCenterMVC.Services;
+using System.Threading.Tasks;
 
 namespace EnglishCenterMVC.Areas.Student.Controllers
 {
+    [Area("Student")]
     public class SubmissionController : Controller
     {
+        ISubmissionService submissionService;
+
+        public SubmissionController(ISubmissionService submissionService)
+        {
+            this.submissionService = submissionService;
+        }
+
         public IActionResult Index()
         {
             return View();
