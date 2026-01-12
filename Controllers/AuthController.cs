51fd22851bc34f68051e0ead2813b98d77831cd4
linhtruong2511 <linhtk251104@gmail.com>
2026-01-12 19:47:09 +0700

    Fix login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using EnglishCenterMVC.Authorization;
+using EnglishCenterMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace EnglishCenterMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            try
+            {
+                var result = await authService.Login(email, password);
+                if (result.Roles.Contains(Roles.ADMIN.ToString()))
+                {
+                    return Redirect("/Admin");
+                }
+                else if (result.Roles.Contains(Roles.STUDENT.ToString()))
+                {
+                    return Redirect("/Student");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Thông tin ??ng nh?p không chính xác";
+            }
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                await authService.Logout(User);
+                return Redirect("/");
+            }
+            catch (Exception ex)
+            {
+            }
+            return View("/");
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,41 +10,17 @@ namespace EnglishCenterMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IAuthService authService;
+        
 
         public HomeController(ILogger<HomeController> logger,
             IAuthService authService)
         {
             _logger = logger;
-            this.authService = authService;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(string email, string password)
-        {
-            try
-            {
-                var result = await authService.Login(email, password);
-                if (result.Roles.Contains(Roles.ADMIN.ToString()))
-                {
-                    return Redirect("/Admin");
-                }
-                else if (result.Roles.Contains(Roles.STUDENT.ToString()))
-                {
-                    return Redirect("/Student");
-                }
-            }
-            catch (Exception ex)
-            {
-                TempData["Error"] = "Thông tin ??ng nh?p không chính xác";
-            }
-            return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
